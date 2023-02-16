eaff19d6fb3177dd2a31f09f2a504f022a54ecd7
C(m,k) <119834826+cmk114514@users.noreply.github.com>
2023-02-16 10:15:09 +0800

    Add files via upload
    Fixed bugs.

--- get_at.py
+++ get_at.py
@@ -1,54 +1,63 @@
 from bs4 import BeautifulSoup as bs
 import requests as req
 from os.path import join as pjoin
-import re
-stitle = re.compile(r'Sample (In|Out)put \d+')
+import re, time
+stitle = re.compile(r'(入|出)力例\s?\d+')
 def get(cid, pid, url):
-	print(f'Getting {cid}{pid}...')
-	try:
-		res = req.get(url, headers = {
-			'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Tablet PC 2.0; wbx 1.0.0; wbxapp 1.0.0; Zoom 3.6.0)'
-		})
-		s = bs(res.text, 'lxml')
-		sa = s.select('#task-statement > span > span.lang-en > div > section')
-		tsa = []
-		for i in sa:
-			if stitle.match(i.find('h3').get_text()):
-				tsa.append(i.find('pre'))
-		assert len(tsa) % 2 == 0
-		ret = []
-		for i in range(0, len(tsa), 2):
-			ret.append((tsa[i].get_text(), tsa[i + 1].get_text()))
-		return ret
-	except Exception as e:
-		print(f'Error {e}')
-		return []
+    print(f'Getting {cid}{pid}...')
+    try:
+        res = req.get(url, headers = {
+            'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Tablet PC 2.0; wbx 1.0.0; wbxapp 1.0.0; Zoom 3.6.0)'
+            })
+        time.sleep(0.1)
+        s = bs(res.text, 'lxml')
+        sa = s.select('#task-statement > div > section')
+        if sa == []:
+            sa = s.select('#task-statement > span > span.lang-ja > div > section')
+        if sa == []:
+            sa = s.select('#task-statement > span > span.lang-jp > div > section')
+        if sa == []:
+            return []
+        tsa = []
+        for i in sa:
+            if stitle.match(i.find('h3').get_text()):
+                tsa.append(i.find('pre'))
+        assert len(tsa) % 2 == 0
+        ret = []
+        for i in range(0, len(tsa), 2):
+            ret.append((tsa[i].get_text(), tsa[i + 1].get_text()))
+        return ret
+    except Exception as e:
+        print(f'Error {e}')
+        return []
 def save(cid, pid, sam):
-	print(f'Saving {cid}{pid}...')
-	try:
-		for i in range(len(sam)):
-			with open(pjoin('at', f'{cid}_{pid}_{i + 1}.in'), 'w') as f:
-				f.write(sam[i][0].replace('\r', '').strip())
-			with open(pjoin('at', f'{cid}_{pid}_{i + 1}.out'), 'w') as f:
-				f.write(sam[i][1].replace('\r', '').strip())
-	except Exception as e:
-		print(f'Error {e}')
+    print(f'Saving {cid}{pid}...')
+    try:
+        for i in range(len(sam)):
+            with open(pjoin('at', f'{cid}_{pid}_{i + 1}.in'), 'w') as f:
+                f.write(sam[i][0].replace('\r', '').strip())
+            with open(pjoin('at', f'{cid}_{pid}_{i + 1}.out'), 'w') as f:
+                f.write(sam[i][1].replace('\r', '').strip())
+    except Exception as e:
+        print(f'Error {e}')
 def get_and_save(cid):
-	print(f'Getting {cid}...')
-	try:
-		url = f'https://atcoder.jp/contests/{cid}/tasks'
-		res = req.get(url, headers = {
-			'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Tablet PC 2.0; wbx 1.0.0; wbxapp 1.0.0; Zoom 3.6.0)'
-		})
-		s = bs(res.text, 'lxml')
-		p = s.select('#main-container > div.row > div:nth-child(2) > div > table > tbody > tr > td:nth-child(1) > a')
-		for i in p:
-			pid = i.get_text().strip().lower()
-			lk = i.get('href')
-			save(cid, pid, get(cid, pid, 'https://atcoder.jp' + lk))
-	except Exception as e:
-		print(f'Error {e}')
+    print(f'Getting {cid}...')
+    try:
+        url = f'https://atcoder.jp/contests/{cid}/tasks'
+        res = req.get(url, headers = {
+            'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Tablet PC 2.0; wbx 1.0.0; wbxapp 1.0.0; Zoom 3.6.0)'
+            })
+        time.sleep(0.1)
+        s = bs(res.text, 'lxml')
+        p = s.select('#main-container > div.row > div:nth-child(2) > div > table > tbody > tr > td:nth-child(1) > a')
+        for i in p:
+            lk = i.get('href')
+            tmp = lk.split('/')
+            rcid, pid = tmp[2], tmp[4].split('_')[-1]
+            save(rcid, pid, get(rcid, pid, 'https://atcoder.jp' + lk))
+    except Exception as e:
+        print(f'Error {e}')
 t, l, r = input('Type & From ?~?: ').split()
 l, r = int(l), int(r)
 for i in range(l, r + 1):
-	get_and_save('%s%03d' % (t, i))
+    get_and_save('%s%03d' % (t, i))
